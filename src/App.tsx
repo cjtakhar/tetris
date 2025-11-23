7943803c94c16b763fc871a9bc92dac9e28ea731
cjtakhar <cjtakhar@gmail.com>
2025-11-22 23:06:11 -0500

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Routes, Route } from "react-router-dom";
 import TetrisGame from "./TetrisGame";
 
 export default function App() {
-  return <TetrisGame />;
+  return (
+    <Routes>
+      {/* main game at /tetris/ */}
+      <Route path="/" element={<TetrisGame />} />
+      {/* example extra route if you want one later */}
+      {/* <Route path="/about" element={<div>About this Tetris clone</div>} /> */}
+    </Routes>
+  );
 }
