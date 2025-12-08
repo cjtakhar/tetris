b77242c91f3f8f3b6c8bf92754d797b022a02cb2
cjtakhar <cjtakhar@gmail.com>
2025-12-08 16:39:24 -0500

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,7 @@ import TetrisGame from "./TetrisGame";
 export default function App() {
   return (
     <Routes>
-      {/* main game at /tetris/ */}
-      <Route path="/" element={<TetrisGame />} />
-      {/* example extra route if you want one later */}
-      {/* <Route path="/about" element={<div>About this Tetris clone</div>} /> */}
+      <Route path="/tetris" element={<TetrisGame />} />
     </Routes>
   );
 }
